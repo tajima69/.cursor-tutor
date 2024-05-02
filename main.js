2db883391783f469bd8ce9bfba923a663a9ee250
tajima69 <kukanakal@gmail.com>
2024-05-02 17:58:34 +0500

    Co-authored-by: Kr1pochek <Kr1pochek@users.noreply.github.com>

--- main.js
+++ main.js
@@ -3,31 +3,60 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-const images = document.querySelectorAll('.slider img');
+const midle = document.querySelector('.slides');
+const slider = document.querySelector('.slide');
+const prevBtn = document.querySelector('.prev-button');
+const nextBtn = document.querySelector('.next-button');
 let currentIndex = 0;
 
-function showImage(index) {
-  images[currentIndex].classList.remove('active');
-  images[index].classList.add('active');
-  currentIndex = index;
+function prevSlide() {
+  currentIndex = currentIndex ===0 ? slides.lenght - 1 : currentIndex - 1;
+
+  updateSliderPosition();
 }
 
-document
-  .querySelector('.slider.controlls')
-  .addEventListener('click', function (event) {
-    if (event.target.classList.contains('prev')) {
-      let index = currentIndex - 1;
-      if (index < 0) {
-        index = images.length - 1;
-      }
-      showImage(index);
-    } else if (event.target.classList.contains('next')) {
-      let index = currentIndex + 1;
-      if (index >= images.length) {
-        index = 0;
-      }
-      showImage(index);
-    }
-  });
+function nextSlide() {
+  currentIndex = currentIndex === slides.lenght - 1 ? 0 : currentIndex + 1;
 
-showImage(currentIndex);
+  updateSliderPosition();
+}
+
+
+function updateSliderPosition(){
+
+const Newposition = -currentIndex * slides[0].offsetWidth
+
+midle.style.transform = 'translateX(${newPosition}px)';
+}
+
+prevBtn.addEventListener('click',prevSlide);
+nextBtn.addEventListener('click',nextSlide);
+
+
+const korzina = document.querySelector("#burger-icon");
+const closeIcon = document.querySelector("#close-icon");
+const menu = document.querySelector("#menu")
+
+console.log(burgerIcon);
+console.log(closeIcon);
+console.log(menu)
+
+burgerIcon.addEventListener("click",function () {
+  menu.style.down = "0";
+})
+
+closeIcon.addEventListener("click",function () {
+  menu.style.down = "-100%";
+})
+
+document.addEventListener("click",function (event) {
+  if (!menu.contains(event.target) && event.target !== burgerIcon) {
+    menu.style.down = "-100%"
+  }
+})
+
+window.addEventListener("click",function () {
+  if (window.innerWidth > 768) {
+    menu.style.down = "-100%";
+  }
+});
