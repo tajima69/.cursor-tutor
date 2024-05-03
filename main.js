434b6947e3b803f7b89e790ef4da76b8e9aabd72
tajima69 <kukanakal@gmail.com>
2024-05-03 23:20:04 +0500

    Co-authored-by: Kr1pochek <Kr1pochek@users.noreply.github.com>

--- main.js
+++ main.js
@@ -3,60 +3,32 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-const midle = document.querySelector('.slides');
-const slider = document.querySelector('.slide');
-const prevBtn = document.querySelector('.prev-button');
-const nextBtn = document.querySelector('.next-button');
-let currentIndex = 0;
+let slideIndex = 0;
+showSlides();
 
-function prevSlide() {
-  currentIndex = currentIndex ===0 ? slides.lenght - 1 : currentIndex - 1;
-
-  updateSliderPosition();
-}
-
-function nextSlide() {
-  currentIndex = currentIndex === slides.lenght - 1 ? 0 : currentIndex + 1;
-
-  updateSliderPosition();
-}
-
-
-function updateSliderPosition(){
-
-const Newposition = -currentIndex * slides[0].offsetWidth
-
-midle.style.transform = 'translateX(${newPosition}px)';
-}
-
-prevBtn.addEventListener('click',prevSlide);
-nextBtn.addEventListener('click',nextSlide);
-
-
-const korzina = document.querySelector("#burger-icon");
-const closeIcon = document.querySelector("#close-icon");
-const menu = document.querySelector("#menu")
-
-console.log(burgerIcon);
-console.log(closeIcon);
-console.log(menu)
-
-burgerIcon.addEventListener("click",function () {
-  menu.style.down = "0";
-})
-
-closeIcon.addEventListener("click",function () {
-  menu.style.down = "-100%";
-})
-
-document.addEventListener("click",function (event) {
-  if (!menu.contains(event.target) && event.target !== burgerIcon) {
-    menu.style.down = "-100%"
+function showSlides() {
+  const slides = document.querySelectorAll('.slider img');
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';  
   }
-})
-
-window.addEventListener("click",function () {
-  if (window.innerWidth > 768) {
-    menu.style.down = "-100%";
+  
+  slideIndex++;
+  
+  if (slideIndex > slides.length) { 
+    slideIndex = 1; 
   }
+  
+  
+  slides[slideIndex - 1].style.display = 'block';  
+  setTimeout(showSlides, 20000); // Change image every 2 seconds
+}
+
+document.querySelector('.prev').addEventListener('click', () => {
+  slideIndex--;
+  showSlides();
+});
+document.querySelector('.next').addEventListener('click', () => {
+  slideIndex++;
+  showSlides();
 });
